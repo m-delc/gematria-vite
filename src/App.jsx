3abe90a60aa3ddb6db96c0e9e71ba1ad9fd3f25d
Michael Delcambre <delcambrem@gmail.com>
2022-04-27 18:18:05 -0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello Vite + React!</p>
+        <p>
+
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and  to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
