ee1fd2405c24720a41ca3b92ae1835041e462d6d
Michael Delcambre <delcambrem@gmail.com>
2022-04-27 19:15:47 -0500

    commit before adding rails

--- src/Components/Input1.jsx
+++ src/Components/Input1.jsx
@@ -2,7 +2,18 @@ import React, { useState } from "react";
 import "../App.css";
 
 const Input1 = () => {
+
+  const letternumber = {
+    a: "1",
+  };
+
   const [inputtest, setinputtest] = useState("");
+
+  const aaa = inputtest.split('')
+//   console.log(aaa)
+//   console.log(inputtest)
+
+
   return (
     <div>
       <input
@@ -11,7 +22,9 @@ const Input1 = () => {
         onChange={(e) => setinputtest(e.target.value)}
       ></input>
       <br></br>
-      {inputtest}
+      {inputtest.split("")}
+      <br></br>
+      {aaa}
     </div>
   );
 };
